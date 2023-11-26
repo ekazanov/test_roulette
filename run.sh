e792d471796b9006f6e5ee5032843804b0c657d4
Evgeny Kazanov <evgeny.kazanov@gmail.com>
2023-11-26 18:53:04 +0000

    Add samples to run.sh.

--- run.sh
+++ run.sh
@@ -2,5 +2,11 @@
 
 source venv/bin/activate
 
-python3 check_roulette.py
+# Get help message
+# python3 check_roulette.py --help
 
+# Try 1 session. Read verbose reports.
+# python3 check_roulette.py --wallet 50 --first_bet 2 --goal_amount 70 --session_num 1 --verbose 1
+
+# Try 100 sessions.
+python3 check_roulette.py --wallet 50 --first_bet 2 --goal_amount 70 --session_num 10 --verbose 0
