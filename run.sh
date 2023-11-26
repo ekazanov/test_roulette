ab8bfa352d44c3abf55009badf3a4cd6d3d2edac
Evgeny Kazanov <evgeny.kazanov@gmail.com>
2023-11-26 19:18:26 +0000

    Add invocation examples.

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 source venv/bin/activate
 
 # Get help message
-# python3 check_roulette.py --help
+python3 check_roulette.py --help
 
 # Try 1 session. Read verbose reports.
 # python3 check_roulette.py --wallet 50 --first_bet 2 --goal_amount 70 --session_num 1 --verbose 1
 
 # Try 100 sessions.
-python3 check_roulette.py --wallet 50 --first_bet 2 --goal_amount 70 --session_num 10 --verbose 0
+# python3 check_roulette.py --wallet 50 --first_bet 2 --goal_amount 70 --session_num 10 --verbose 0
